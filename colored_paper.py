c45bf7048c5daf7e433fc50e191e4c01ecd5e321
byeongjulee222 <52685247+byeongjulee222@users.noreply.github.com>
2019-08-19 22:50:31 +0900

    190819

--- colored_paper.py
+++ colored_paper.py
@@ -1,34 +1,26 @@
+import sys
+sys.stdin = open('colored_paper.txt', 'r')
+
 N = int(input())
+# 범위 : 가로, 세로 최대 101칸의 정사각형
+square = [[0] * 101 for _ in range(101)]
 
-# for i in range(N-1):
+# 색종이 장 수만큼 반복
+for n in range(1, N+1):
+    # 시작점(a, b), 가로 너비, 세로 너비
+    a, b, c, d = list(map(int, input().split()))
+    # 첫 번째 색종이 += 1, 두 번째 색종이 += 2, ...
+    for i in range(c):
+        for j in range(d):
+            square[a+i][b+j] = n
 
-i = 0
-a, b, c, d = map(int, input().split())
-lst1 = [(x, y) for x in range(a, a+c) for y in range(b, b+d)]
-while i < N:
-    i += 1
-    for j in range(1, N):
-        e, f, g, h = map(int, input().split())
-        lst2 = [(x, y) for x in range(e, e+g) for y in range(f, f+h)]
+# 결과값을 저장할 리스트 만들기
+result = [0] * N
+for num in square:
+    for foll in num:
+        # 사각형 중 그 값에 해당하는 값이 있으면 결과값 리스트에 저장
+        if foll:
+            result[foll-1] += 1
 
-        # for t in lst1:
-        #     if t in lst2:
-        #         del lst1.remove(t)
-        result = len([x for x in lst1 if x not in lst2])
-    print(result)
-print(len(lst2))
-
-# result = []
-# num_list = []
-# alpha = ['a', 'b', 'c', 'd']
-#     num_list.append(input().split())
-
-# # print(num_list)
-    
-#     lst1 = {}
-#     for j in range(4):
-#         lst1[alpha[j]] = num_list[i][j]
-#     result.append(lst1)
-# print(result)
-
-# for i in range(N):
+for i in range(len(result)):
+    print(result[i])
