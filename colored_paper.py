66e42a4ed12ff7facacf329d39859ac6fa9e3e77
byeongjulee222 <dave.juya777@gmail.com>
2019-08-19 13:48:05 +0900

    190819

--- colored_paper.py
+++ colored_paper.py
@@ -1,22 +1,26 @@
 N = int(input())
 
 # for i in range(N-1):
-result = []
+
+i = 0
 a, b, c, d = map(int, input().split())
 lst1 = [(x, y) for x in range(a, a+c) for y in range(b, b+d)]
+while i < N:
+    i += 1
+    for j in range(1, N):
+        e, f, g, h = map(int, input().split())
+        lst2 = [(x, y) for x in range(e, e+g) for y in range(f, f+h)]
 
-for j in range(1, N):
-    e, f, g, h = map(int, input().split())
-    lst2 = [(x, y) for x in range(e, e+g) for y in range(f, f+h)]
-
-    result = len([x for x in lst1 if x not in lst2])
+        for t in lst1:
+            if t in lst2:
+                del lst1.remove(t)
+        # result = len([x for x in lst1 if x not in lst2])
     print(result)
 print(len(lst2))
 
 # result = []
 # num_list = []
 # alpha = ['a', 'b', 'c', 'd']
-# for i in range(N):
 #     num_list.append(input().split())
 
 # # print(num_list)
