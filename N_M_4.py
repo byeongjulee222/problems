def41dc32b5b4c3d590277b82d1b250c43d28dbb
byeongjulee222 <52685247+byeongjulee222@users.noreply.github.com>
2019-09-30 01:10:39 +0900

    190930

--- N_M_4.py
+++ N_M_4.py
@@ -1,11 +1,11 @@
 def comb(k, s):
-    if k == M:
+    if len(choose) == 2:
         print(*choose)
         return
 
     for i in range(s, N):
         choose.append(arr[i])
-        comb(k+1, i+1)
+        comb(k+1, i)
         choose.pop()
 
 N, M = map(int, input().split())
