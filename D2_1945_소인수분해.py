f37b84b0aa2935573e2a70bfd9e067ed938b5b1d
byeongjulee222 <dave.juya777@gmail.com>
2019-11-21 17:14:13 +0900

    191121

---
+++ D2_1945_소인수분해.py
@@ -0,0 +1,23 @@
+import sys; sys.stdin = open('txt/D2_1945_소인수분해.txt', 'r')
+
+for tc in range(1, int(input())+1):
+    N = int(input())
+    a = b = c = d = e = 0
+    while N > 1:
+        if not N % 2:
+            a += 1
+            N //= 2
+        if not N % 3:
+            b += 1
+            N //= 3
+        if not N % 5:
+            c += 1
+            N //= 5
+        if not N % 7:
+            d += 1
+            N //= 7
+        if not N % 11:
+            e += 1
+            N //= 11
+
+    print('#{} {} {} {} {} {}'.format(tc, a, b, c, d, e))
