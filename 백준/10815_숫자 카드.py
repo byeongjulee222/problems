ff62ff536514cc2e23edde90a2054df53508e607
juya <dave.juya777@gmail.com>
2020-01-09 00:18:29 +0900

    200109

--- 백준/10815_숫자 카드.py
+++ 백준/10815_숫자 카드.py
@@ -1,15 +1,14 @@
 import sys; sys.stdin = open('txt/10815_숫자 카드.txt' ,'r')
 
 N = int(input())
-arr1 = list(map(int, input().split()))
+# 원 배열을 list말고 set으로 받으니까 통과
+# list로 제출하면 시간초과
+arr1 = set(map(int, input().split()))
 M = int(input())
 arr2 = list(map(int, input().split()))
 
-result = []
 for m in arr2:
     if m in arr1:
-        result.append(1)
+        print(1, end=' ')
     else:
-        result.append(0)
-
-print(*result)
+        print(0, end=' ')
--- 백준/7568_덩치.py
+++ 백준/7568_덩치.py
@@ -1,3 +1,7 @@
+import sys; sys.stdin = open('txt/7568_덩치.txt', 'r')
+# 단순히 한 명을 선택해서 배열을 도는데
+# 키, 몸무게 다 크면 rank += 1
+
 N = int(input())
 
 arr = []
@@ -5,3 +9,11 @@ for _ in range(N):
     a, b = map(int, input().split())
     arr.append((a, b))
 
+# print(arr)
+
+for person in arr:
+    rank = 1
+    for compare in arr:
+        if person[0] < compare[0] and person[1] < compare[1]:
+            rank += 1
+    print(rank, end=' ')
