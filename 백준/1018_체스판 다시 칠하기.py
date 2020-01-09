78f7a81590da16eb008cbf2274cb5f8990f691d4
juya <dave.juya777@gmail.com>
2020-01-09 23:16:52 +0900

    200109

--- 백준/1018_체스판 다시 칠하기.py
+++ 백준/1018_체스판 다시 칠하기.py
@@ -3,28 +3,46 @@ from pprint import pprint
 # 전체 판에서 8*8 크기로 잘라서
 # 체스판을 만들 때 가장 적게 바꿀때의 횟수
 
-def chess(a, b):
-    pass
+A = [['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+ ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+ ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+ ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+ ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+ ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+ ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+ ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']]
 
-arr1 = [[1, 2, 3], [2, 3, 4]]
-arr2 = [[1, 2, 3], [2, 3, 5]]
+B = [['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+ ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+ ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+ ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+ ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+ ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+ ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+ ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B']]
 
-#
 # for tc in range(1, int(input())+1):
-#     N, M = map(int, input().split())
-#
-#     arr = []
-#     for _ in range(N):
-#         arr.append(list(input()))
-#
-#     for i in range(N):
-#         for j in range(M):
-#             Min = 0xffffff
-#             for row in range(8):
-#                 for col in range(8):
-#                     pass
-#
-#     pprint(arr)
-#     break
+N, M = map(int, input().split())
 
-print(arr1 == arr2)
+arr = []
+for _ in range(N):
+    arr.append(list(input()))
+c = 0
+Min = 0xffffff
+for a in range(N-7):
+    for b in range(M-7):
+        cnt_A, cnt_B = 0, 0
+        for i in range(8):
+            for j in range(8):
+                if A[i][j] != arr[a+i][b+j]: cnt_A += 1
+                if B[i][j] != arr[a+i][b+j]: cnt_B += 1
+        # print('cnt_A :', cnt_A)
+        # print('cnt_B :', cnt_B)
+        # c += 1
+        # print('c :', c)
+        Min = min(cnt_A, cnt_B, Min)
+        # print('Min :', Min)
+
+print(Min)
+# print('----------')
+# break
