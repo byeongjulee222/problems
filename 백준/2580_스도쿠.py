6705d327a04b180c0f60901d077d975e38305bd4
multicampus <dave.juya777@gmail.com>
2020-01-10 17:56:54 +0900

    200110

---
+++ 백준/2580_스도쿠.py
@@ -0,0 +1,12 @@
+import sys; sys.stdin = open('txt/2580_스도쿠.txt', 'r')
+
+numbers = [i for i in range(1, 10)]
+
+arr = []
+for _ in range(9):
+    arr.append(list(map(int, input().split())))
+
+for i in range(9):
+    if arr[i].count(0) == 1:
+        for j in sorted(arr[i]):
+            if j == numbers[j-1]: pass
