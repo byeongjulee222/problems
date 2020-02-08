a1bc86bdf63721b13b7367f9654d3463a8859e91
juya <dave.juya777@gmail.com>
2020-02-08 18:32:55 +0900

    200208 | 캐슬디펜스, 감시// 복습 + 정리

--- 백준/15683_감시.py
+++ 백준/15683_감시.py
@@ -1,52 +1,61 @@
 import sys; sys.stdin = open('txt/15683_감시.txt', 'r')
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-#     동 서 남  북
+#  동, 서, 남, 북 순서
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+# cctv가 보는 방향을 cctv 종류에 따라 저장
 dir = [0,
        [[0], [1], [2], [3]],
        [[0, 1], [2, 3]],
-       [[0, 3], [0, 2], [1, 2], [1, 3]],
+       [[0, 2], [0, 3], [1, 2], [1, 3]],
        [[0, 1, 2], [0, 2, 3], [0, 1, 3], [1, 2, 3]],
        [[0, 1, 2, 3]]
        ]
 
 def watch(x, y, lst, val):
+    # cctv가 바라보는 방향 경우 중 한 방향씩 체크
     for d in lst:
         nx, ny = x+dx[d], y+dy[d]
-        while 0<=nx<N and 0<=ny<M and arr[nx][ny] != 6:
+        # 경계 조건 + 벽을 만났을 경우 제외 조건
+        while 0 <= nx < N and 0 <= ny < M and arr[nx][ny] != 6:
             visit[nx][ny] += val
-            nx += dx[d]
-            ny += dy[d]
+            # 체크하는 방향으로 쭉 체크
+            nx, ny = nx+dx[d], ny+dy[d]
 
 def backtrack(k, n):
     global Min
+    # cctv 개수만큼 반복(종료 조건)
     if k == n:
         cnt = 0
         for i in range(N):
             for j in range(M):
+                # watch가 끝난 후 그 자리가 0이고, 방문한 적 없는 경우에만 count
                 if arr[i][j] == 0 and not visit[i][j]:
                     cnt += 1
         Min = min(Min, cnt)
     else:
+        # cctv 좌표와 종류 불러옴
         x, y, d = cctv[k]
+        # cctv 종류별 바라보는 방향 반복
         for lst in dir[d]:
             watch(x, y, lst, 1)
             backtrack(k+1, n)
+            # 끝까지 내려갔을 때 되돌아오기(backtracking)
             watch(x, y, lst, -1)
-
 
 for _ in range(int(input())):
     N, M = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
+
+    # cctv 값 저장(좌표값, 종류 번호)
     cctv = []
     for i in range(N):
         for j in range(M):
-            if 0 < arr[i][j] < 6:
-                cctv.append((i, j, arr[i][j]))
-    # print(cctv)
-    # break
+            if 0 < arr[i][j] < 6: cctv.append((i, j, arr[i][j]))
+
+    Min = 0xffffff
     visit = [[0]*M for _ in range(N)]
-    Min = N*M
+    # cctv 리스트 원소 개수만큼 반복
     backtrack(0, len(cctv))
     print(Min)
--- 백준/17135_캐슬디펜스.py
+++ 백준/17135_캐슬디펜스.py
@@ -6,57 +6,53 @@ from copy import deepcopy
 for tc in range(1, int(input())+1):
     N, M, D = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
-    castle = [i for i in range(N)]
+    castle = [i for i in range(M)]
 
     ans = 0
-    Max = 0xfffffff
-    enemy = deque()
-
+    Max = 0xffffff
+    # 조합의 경우 모두 확인
     for archers in combinations(castle, 3):
-        Map = deepcopy(arr)
+        # 적을 큐(스택)에 담아서 하나씩 꺼내며 처리
+        # 경우마다 킬 수는 0으로 초기화
+        # 경우마다 Map을 원래의 arr로 deepcopy
+        enemy = deque()
         killed = 0
+        Map = deepcopy(arr)
+        # 종료조건 만족할 때까지 반복
         while True:
-            # 적 위치 저장
+            # Map에 0이 아닌 값이 있으면 적
             for i in range(N):
                 for j in range(M):
-                    if Map[i][j]:
-                        enemy.append((i, j))
-
-            # 종료 조건(Map에 적이 없을 때)
-            if len(enemy) == 0: break
-
-            # 최소 거리에 있는 적을 찾기위해 Max 거리를 최대로 지정
-            # target은 번호 순서대로 궁수 번호와 맞춰서 몇 번 궁수가 어떤 적(좌표)를 선택했는지 저장
+                    if Map[i][j]: enemy.append((i, j))
+            # 적이 없으면 종료
+            if not len(enemy): break
+            # 적을 타겟으로 선정할지 판단하기 위해(가까운 적 선택하기 위해 첫 조건은 큰 값)
+            # 그 때의 거리값도 저장해서 거리가 같은 적이 있을 때 왼쪽 적을 선택함
             target = [(0, Max) for _ in range(3)]
             dist = [Max for _ in range(3)]
 
-            # enemy(deque)에 적이 있는 경우 계속 진행
+            # 적을 모두 판단하여 처리할 때까지 진행
             while enemy:
                 x, y = enemy.popleft()
-                # 궁수 3명 반복
+                # 궁수 3명이므로 3번 반복
                 for i in range(3):
-                    # 궁수 행은 N으로 고정, 열은 archers[i](조합)
-                    d = abs(N - x) + abs(archers[i] - y)
-                    # 사정거리 안에 들어왔을 때
-                    if dist[i] > d:
-                        # 거리를 d로 정해두고
+                    # 궁수와 적 거리값을 미리 변수화해둠
+                    d = abs(N-x) + abs(archers[i] - y)
+                    # 사정거리 내에 적이 들어왔을 때 타겟으로 선정(좌표 저장)
+                    if d < dist[i]:
                         dist[i] = d
-                        # i번째 궁수의 타겟(좌표)을 저장
                         target[i] = (x, y)
-
-                    # 사정거리 안에 있는 적이 여럿일 경우 y가 작은 거을 타겟으로 선정
+                    # 사정거리가 같을 때, 왼쪽편에 있는 적으로 타겟 변경
                     if dist[i] == d and y < target[i][1]:
                         target[i] = (x, y)
 
-            # 궁수별로 타겟을 다 정한 후
-            #
             for i, (x, y) in enumerate(target):
-                if Map[x][y]:
-                    if dist[i] <= D:
-                        Map[x][y] = 0
-                        killed += 1
-
-            Map = [[0] * M] + Map[:N-1]
+                # Map에 적이 있고, 그 적이 사정거리 안에 있는지 다시 확인
+                if Map[x][y] and dist[i] <= D:
+                    # Map에 값을 0으로 바꾸고 kill 수 증가
+                    Map[x][y] = 0
+                    killed += 1
+            # 밑으로 한칸 내림
+            Map = [[0]*M] + Map[:N-1]
         ans = max(ans, killed)
-
     print(ans)
