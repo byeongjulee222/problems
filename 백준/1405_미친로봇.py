728779b9bc88d5cd4e69b7cc33928fdb4b1c4a64
juya <dave.juya777@gmail.com>
2020-02-11 08:09:47 +0900

    200211

--- 백준/1405_미친로봇.py
+++ 백준/1405_미친로봇.py
@@ -3,25 +3,25 @@ import sys; sys.stdin = open('txt/1405_미친로봇.txt', 'r')
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
-def dfs(x, y, move, prob):
+def backtrack(x, y, move, prob):
     global ans, visit
     if move == N:
         if len(set(visit)) == N+1:
             ans += prob
         return
 
-    for a in range(4):
-        nx, ny = x+dx[a], y+dy[a]
-        # 방문한 곳이 아니면 한 번 더 전진
-        # 그 방향으로의 확률도 곱해준다
+    for i in range(4):
+        nx, ny = x+dx[i], y+dy[i]
         if (nx, ny) not in visit:
             visit.append((nx, ny))
-            dfs(nx, ny, move+1, prob*p[a])
+            backtrack(nx, ny, move+1, prob*P[i])
             visit.pop()
 
 N, p_e, p_w, p_s, p_n = map(int, input().split())
-p = [p_e/100, p_w/100, p_s/100, p_n/100]
+P = [p_e/100, p_w/100, p_s/100, p_n/100]
 ans = 0
 visit = [(0, 0)]
-dfs(0, 0, 0, 1)
+backtrack(0, 0, 0, 1)
+# 소수점 아래 10번째 자리까지 표시
 print('{:.10f}'.format(ans))
+print('%.10f' % ans)
