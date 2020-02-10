aa441d5680d335050809561119819180edb050b5
multicampus <dave.juya777@gmail.com>
2020-02-11 08:46:44 +0900

    200211

--- 백준/14501_퇴사 OK.py
+++ 백준/14501_퇴사 OK.py
@@ -1,19 +1,5 @@
 import sys; sys.stdin = open('txt/14501_퇴사.txt', 'r')
-'''
-for tc in range(1, int(input())+1):
-    N = int(input())
-    arr = list(list(map(int, input().split())) for _ in range(N))
-    dp = [0] * N
 
-    for i in range(N):
-        if i + arr[i][0] <= N:
-            dp[i] = arr[i][1]
-            for j in range(i):
-                if j + arr[j][0] <= i:
-                    dp[i] = max(dp[i], dp[j] + arr[i][1])
-
-    print(max(dp))
-'''
 for tc in range(1, int(input())+1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
