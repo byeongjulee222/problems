3df214a3db0ba4f1a3dfbbf57777f1bccfaa4aba
multicampus <dave.juya777@gmail.com>
2020-02-11 19:55:47 +0900

    200211

--- 백준/1405_미친로봇.py
+++ 백준/1405_미친로봇.py
@@ -1,18 +1,16 @@
 import sys; sys.stdin = open('txt/1405_미친로봇.txt', 'r')
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
 
 def backtrack(x, y, move, prob):
     global ans, visit
     if move == N:
-        if len(set(visit)) == N+1:
-            ans += prob
+        ans += prob
         return
 
     for i in range(4):
         nx, ny = x+dx[i], y+dy[i]
-        if (nx, ny) not in visit:
+        if not (nx, ny) in visit:
             visit.append((nx, ny))
             backtrack(nx, ny, move+1, prob*P[i])
             visit.pop()
@@ -22,6 +20,4 @@ P = [p_e/100, p_w/100, p_s/100, p_n/100]
 ans = 0
 visit = [(0, 0)]
 backtrack(0, 0, 0, 1)
-# 소수점 아래 10번째 자리까지 표시
 print('{:.10f}'.format(ans))
-print('%.10f' % ans)
