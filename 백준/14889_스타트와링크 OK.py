c9c25d76d9772e1b8606897ff2343194ce9efb83
juya <dave.juya777@gmail.com>
2020-02-27 11:52:41 +0900

    200227 | 스타트와 링크(조합)

--- 백준/14889_스타트와링크 OK.py
+++ 백준/14889_스타트와링크 OK.py
@@ -1,30 +1,27 @@
 import sys; sys.stdin = open('txt/14889_스타트와링크.txt', 'r')
 from itertools import combinations
 
-for tc in range(1, int(input())+1):
+for _ in range(int(input())):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
-
     member = list(range(N))
-    set1 = list(combinations(member, N // 2))
+
     Min = 999999
-    for group in set1:
-        rest = list(set(member) - set(group))
+    # 팀을 두 개로 나누기 (조합)
+    for group1 in combinations(member, N // 2):
+        group2 = list(set(member) - set(group1))
 
-        group_comb = list(combinations(group, 2))
-        group_rest = list(combinations(rest, 2))
+        # 1팀, 2팀 능력치 계산
+        team_1, team_2 = 0, 0
+        for i, j in combinations(group1, 2):
+            team_1 += arr[i][j] + arr[j][i]
+        for i, j in combinations(group2, 2):
+            team_2 += arr[i][j] + arr[j][i]
+        Min = min(Min, abs(team_1 - team_2))
 
-        A, B = 0, 0
-        for (i, j) in group_comb:
-            A += arr[i][j] + arr[j][i]
-        for i, j in group_rest:
-            B += arr[i][j] + arr[j][i]
-        Min = min(Min, abs(A - B))
-    #
     print(Min)
 
 # 두 그룹으로 나누는 문제 코드 구현
-'''
 N = 4
 # acnt, bcnt 를 index로 사용
 def backtrack(k, acnt, bcnt):
@@ -47,4 +44,3 @@ A = [0] * N
 B = [0] * N
 
 backtrack(1, 1, 0)
-'''
