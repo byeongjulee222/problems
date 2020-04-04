ca6e669e3a12bb8e2757f33bdb6ee320adcea32d
multicampus <dave.juya777@gmail.com>
2020-04-04 23:31:49 +0900

    0404 | 장훈이_resolve

--- 1486_장훈이의 높은 선반.py
+++ 1486_장훈이의 높은 선반.py
@@ -1,16 +1,17 @@
 import sys; sys.stdin = open('txt/1486_장훈이의 높은 선반.txt', 'r')
 
-# 모든 경우의 가지치기를 해보고 최소값을 출력
+# 모든 경우를 다 result에 넣고 최소값을 출력
 def find(height, num):
-    # 선반의 물건을 꺼낼 수 있는 높이라면 result에 추가
+    # 선반높이보다 높으면 result에 넣음
     if height >= B:
         result.append(height)
     if num == N: return
 
-    # 더하고 진행
+    # 높이 더하고 진행
     find(height+arr[num], num+1)
-    # 더하지 않고 진행
+    # 높이 더하지않고 진행
     find(height, num+1)
+
 
 for tc in range(1, int(input())+1):
     N, B = map(int, input().split())
