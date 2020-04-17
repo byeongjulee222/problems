83fb94d3dab277016f5b0a3596473654f8047229
multicampus <dave.juya777@gmail.com>
2020-04-18 07:47:28 +0900

    0418 | 빙산 풀이중

--- 백준/2573_빙산.py
+++ 백준/2573_빙산.py
@@ -4,9 +4,23 @@ from pprint import pprint
 # 1년 지날 때마다 cnt만큼 숫자 감소 ( max(0, cnt) )
 # 배열 돌면서 덩어리 개수 찾기
 # 덩어리 개수가 2개 이상될 때 그만
+from collections import deque
 
-# import sys
+import sys
 sys.setrecursionlimit(1000000)
+def bfs(a, b):
+    q = deque()
+    q.append((a, b))
+    while q:
+        x, y = q.popleft()
+        visit[x][y] = True
+        for i in range(4):
+            nx, ny = x+dx[i], y+dy[i]
+            if 0 <= nx < Row and 0 <= ny < Col:
+                if not visit[nx][ny] and arr[nx][ny] != 0:
+                    q.append((nx, ny))
+
+
 def dfs(a, b):
     visit[a][b] = True
     for d in range(4):
@@ -14,52 +28,46 @@ def dfs(a, b):
         if 0 <= nx < Row and 0 <= ny < Col:
             if not visit[nx][ny] and arr[nx][ny] != 0:
                 dfs(nx, ny)
-    return
+    # return
 
 
 # 네 방향에 있는지 찾기
 def find(x, y):
-    global cnt
-    visit[x][y] = True
     for a in range(4):
         nx, ny = x+dx[a], y+dy[a]
         if 0 <= nx < Row and 0 <= ny < Col:
-            if arr[nx][ny] == 0 and not visit[nx][ny]:
-                cnt += 1
-    return
+            if arr[nx][ny] == 0:
+                arr[x][y] -= 1
+                if arr[x][y] == 0: break
+    # return
 
 Row, Col = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(Row)]
-print(arr)
+# print(arr)
 dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 year = 0
 num = 0
 while True:
-    # pprint(arr)
-    # print()
     if num >= 2: break
     num = 0
-    visit = [[False] * Col for _ in range(Row)]
-    for i in range(Row):
-        for j in range(Col):
-            cnt = 0
+    # visit = [[False] * Col for _ in range(Row)]
+    for i in range(1, Row-1):
+        for j in range(1, Col-1):
             if arr[i][j] != 0:
                 find(i, j)
-                arr[i][j] -= cnt
-                arr[i][j] = max(arr[i][j], 0)
+                # arr[i][j] = max(arr[i][j], 0)
     # pprint(arr)
+
     visit = [[False] * Col for _ in range(Row)]
-    for i in range(Row):
-        for j in range(Col):
+    for i in range(1, Row-1):
+        for j in range(1, Col-1):
             if not visit[i][j] and arr[i][j] != 0:
-                dfs(i, j)
+                # dfs(i, j)
+                bfs(i, j)
                 num += 1
-    print('num: ', num)
-    pprint(arr)
+    # print('num: ', num)
+    # pprint(arr)
 
     year += 1
 
-if num:
-    print(year)
-else:
-    print(0)
+print(year if num else 0)
