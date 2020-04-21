58f9b91a6988b087e1dbfdefb7332cf6c8273586
juya <dave.juya777@gmail.com>
2020-04-22 00:39:07 +0900

    200422 | 입국심사(파라메트릭 서치)

--- 백준/2573_빙산.py
+++ 백준/2573_빙산.py
@@ -1,96 +1,5 @@
 import sys; sys.stdin = open('txt/2573_빙산.txt', 'r')
 
-from pprint import pprint
-# 빙산 찾아서 주변에 0 개수 찾아서 저장 (cnt)
-# 1년 지날 때마다 cnt만큼 숫자 감소 ( max(0, cnt) )
-# 배열 돌면서 덩어리 개수 찾기
-# 덩어리 개수가 2개 이상될 때 그만
-# from collections import deque
-# from copy import deepcopy
-#
-# import sys
-# sys.setrecursionlimit(1000000)
-# def bfs(a, b):
-#     q = deque()
-#     q.append((a, b))
-#     visit[a][b] = True
-#     while q:
-#         x, y = q.popleft()
-#         for i in range(4):
-#             nx, ny = x+dx[i], y+dy[i]
-#             if 0 <= nx < Row and 0 <= ny < Col:
-#                 if not visit[nx][ny] and arr[nx][ny] != 0:
-#                     q.append((nx, ny))
-#                     visit[nx][ny] = True
-#
-#
-# def dfs(a, b):
-#     visit[a][b] = True
-#     for d in range(4):
-#         nx, ny = a+dx[d], b+dy[d]
-#         if 0 <= nx < Row and 0 <= ny < Col:
-#             if not visit[nx][ny] and arr[nx][ny] != 0:
-#                 dfs(nx, ny)
-#                 # visit[nx][ny] = True
-#     # return
-#
-#
-# # 네 방향에 있는지 찾기
-# def find(x, y):
-#     for a in range(4):
-#         nx, ny = x+dx[a], y+dy[a]
-#         if 0 <= nx < Row and 0 <= ny < Col:
-#             if arr[nx][ny] == 0:
-#                 ocean_cnt[x][y] += 1
-#     # return
-#
-# # for _ in range(int(input())):
-# Row, Col = map(int, input().split())
-# arr = [list(map(int, input().split())) for _ in range(Row)]
-# # print(arr)
-# dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-# year = 0
-# num = 0
-# while True:
-#     if num >= 2: break
-#     num = 0
-#     # 바다와 접한 면 개수 세기
-#     ocean_cnt = [[0]*Col for _ in range(Row)]
-#     for i in range(1, Row-1):
-#         for j in range(1, Col-1):
-#             if arr[i][j] != 0:
-#                 find(i, j)
-#                 # arr[i][j] = max(arr[i][j], 0)
-#     # pprint(arr)
-#
-#     # 녹이는 작업
-#     for i in range(Row):
-#         for j in range(Col):
-#             arr[i][j] -= ocean_cnt[i][j]
-#             if arr[i][j] < 0: arr[i][j] = 0
-#
-#     # 빙산 개수 세기
-#     visit = [[False] * Col for _ in range(Row)]
-#     for i in range(1, Row-1):
-#         for j in range(1, Col-1):
-#             if not visit[i][j] and arr[i][j] != 0:
-#                 # visit[i][j] = True
-#                 # dfs(i, j)
-#                 bfs(i, j)
-#                 num += 1
-#     # print('num: ', num)
-#     # pprint(arr)
-#
-#     year += 1
-#
-# print(year if num else 0)
-
-# 2, 19
-
-
-# 큐에 빙산 좌표 넣어놓고 뽑다가 더이상 못뽑을 때 그룹 수 += 1
-
-
 import sys
 from collections import deque
 def bfs(x, y, visit):
@@ -136,7 +45,7 @@ while True:
     visit = [[False] * Col for _ in range(Row)]
     for i in range(1, Row-1):
         for j in range(1, Col-1):
-            if arr[i][j] != 0 and visit[i][j] == 0:
+            if arr[i][j] != 0 and not visit[i][j]:
                 cnt += 1
                 bfs(i, j, visit)
 
--- 백준/3079_입국심사.py
+++ 백준/3079_입국심사.py
@@ -1,9 +1,26 @@
 import sys; sys.stdin = open('txt/3079_입국심사.txt', 'r')
 
+# 파라메트릭 서치 알고리즘 적용
+# https://sarah950716.tistory.com/16
 for _ in range(int(input())):
+    # N: 심사대 개수, M: 사람 수
     N, M = map(int, input().split())
     inspect = []
     for _ in range(N):
         inspect.append(int(input()))
 
-    print(inspect)
+    left = 0
+    right = M * max(inspect)
+    while left <= right:
+        mid = (left+right) // 2
+
+        passed = 0
+        for time in inspect:
+            passed += mid // time
+
+        if passed < M:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    print(right+1)
