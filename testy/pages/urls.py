f6aaea445dfeb3c68d109f1dee67b3680780c57f
juya <dave.juya777@gmail.com>
2020-04-22 23:41:04 +0900

    정리

--- testy/pages/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('push/', views.push),
-    path('pull/', views.pull),    
-]
--- testy/testy01/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\student\desktop\testy\testy01\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
