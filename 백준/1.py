3675ac96ffa5f456685a10ff6ccb88abc683f81c
juya <dave.juya777@gmail.com>
2020-05-06 00:22:42 +0900

    200506

--- 백준/1.py
+++ 백준/1.py
@@ -1,7 +1,26 @@
-N = int(input())
+def gapInsertionSort(x, start, gap):
+    for target in range(start+gap, len(x), gap):
+        val = x[target]
+        i = target
+        while i > start:
+            if x[i-gap] > val:
+                x[i] = x[i-gap]
+            else:
+                break
+            i -= gap
+        x[i] = val
+
+def shellSort(x):
+    gap = len(x) // 2
+    while gap > 0:
+        for start in range(gap):
+            gapInsertionSort(x, start, gap)
+        gap = gap // 2
+
 lst = []
-for _ in range(N):
+for _ in range(int(input())):
     lst.append(int(input()))
-lst.sort()
+
+shellSort(lst)
 for i in lst:
     print(i)
