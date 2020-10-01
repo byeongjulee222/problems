686296cfb2a6e974cd20bf01eb65251891ae71ff
juya <dave.juya777@gmail.com>
2020-10-01 17:50:12 +0900

    201001 | 숨바꼭질

---
+++ 백준/1697_숨바꼭질.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+n, k = map(int, input().split())
+Max = 100001
+arr = [0 for _ in range(Max)]
+
+def bfs():
+    q = deque()
+    q.append(n)
+    while q:
+        now_pos = q.popleft()
+        if now_pos == k:
+            return arr[now_pos]
+        for next_pos in [now_pos - 1, now_pos + 1, now_pos * 2]:
+            if 0 <= next_pos < Max and not arr[next_pos]:
+                arr[next_pos] = arr[now_pos] + 1
+                q.append(next_pos)
+
+print(bfs())
