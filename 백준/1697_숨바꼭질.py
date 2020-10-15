d0a0096f8424948127e7473472b66f40216f7efa
juya <dave.juya777@gmail.com>
2020-10-15 21:27:44 +0900

    201015 | problem added

--- 백준/1697_숨바꼭질.py
+++ 백준/1697_숨바꼭질.py
@@ -1,3 +1,4 @@
+import sys; sys.stdin = open('txt/1697_숨바꼭질.txt', 'r')
 from collections import deque
 
 n, k = map(int, input().split())
@@ -10,6 +11,7 @@ def bfs():
     while q:
         now_pos = q.popleft()
         if now_pos == k:
+            print(arr)
             return arr[now_pos]
         for next_pos in [now_pos - 1, now_pos + 1, now_pos * 2]:
             if 0 <= next_pos < Max and not arr[next_pos]:
--- 백준/17070_파이프 옮기기.py
+++ 백준/17070_파이프 옮기기.py
@@ -2,14 +2,27 @@
 
 import sys; sys.stdin = open('txt/17070_파이프 옮기기.txt', 'r')
 
+def dfs(x, y, z):  # → 0, ↘ 1, ↓ 2
+    global k
+    if x == n-1 and y == n-1: k += 1
+    if z == 0 or z == 1:  # 파이프 방향 확인
+        if y + 1 < n:  # 맵을 넘지 못하게 설정
+            if Map[x][y+1] == 0:  # 칸에 벽이 없는지 확인
+                dfs(x, y+1, 0)
+    if z == 0 or z == 1 or z == 2:
+        if x+1 < n and y+1 < n:
+            if Map[x+1][y] == Map[x][y+1] == Map[x+1][y+1] == 0:
+                dfs(x+1, y+1, 1)
+    if z == 1 or z == 2:
+        if x + 1 < n:
+            if Map[x+1][y] == 0:
+                dfs(x+1, y, 2)
 
-def pipe():
-    pass
-
-
-for _ in range(int(input())):
-    N = int(input())
-    Map = []
-    for _ in range(N):
-        Map.append(list(map(int,input().split())))
-    print(Map)
+# for _ in range(int(input())):
+n = int(input())
+Map = []
+for _ in range(n):
+    Map.append(list(map(int,input().split())))
+k = 0
+dfs(0, 1, 0)
+print(k)
