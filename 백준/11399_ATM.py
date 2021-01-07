094575749dc1aa18d6416328db15e022a89af2f6
byeongjulee222 <dave.juya777@gmail.com>
2021-01-07 22:57:17 +0900

    210107 | 11399_ATM

--- 백준/11399_ATM.py
+++ 백준/11399_ATM.py
@@ -2,7 +2,7 @@ import sys; sys.stdin = open('txt/11399_ATM.txt', 'r')
 
 nums = int(input())
 clients = sorted(list(map(int, input().split())))
-##
+
 ans = 0
 length = len(clients)
 for time in clients:
