807b965ed67ea01468bc3d4fbb0f52fecd0380aa
juya <dave.juya777@gmail.com>
2021-01-07 22:45:15 +0900

    210107 | 11399_ATM

---
+++ 백준/11399_ATM.py
@@ -0,0 +1,12 @@
+import sys; sys.stdin = open('txt/11399_ATM.txt', 'r')
+
+nums = int(input())
+clients = sorted(list(map(int, input().split())))
+
+ans = 0
+length = len(clients)
+for time in clients:
+    ans += time * length
+    length -= 1
+
+print(ans)
